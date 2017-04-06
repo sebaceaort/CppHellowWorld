ea0c3144a603fca94cd3ca2eb637acefdfef8145
sebaceaort <sceaort@gmail.com>
2017-04-06 14:19:08 -0300

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -1,8 +1,8 @@
 // Nombre: Hello.cpp
-// Alumno : Sebastian Cea
+// Alumno: Sebastian Cea
 //Legajo : 1580048
 // Curso : k1051
-
+//Fecha: 20170403
 #include <iostream>
 int main (){
 std::cout<<("Hello World\n");
